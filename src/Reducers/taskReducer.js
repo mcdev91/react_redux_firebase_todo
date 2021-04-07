cb35adc7e144197f8e73bafbc9e27690fcb118de
martynasdev <43806057+martynasdev@users.noreply.github.com>
2021-04-07 09:22:22 +0300

    toast edit

--- src/Reducers/taskReducer.js
+++ src/Reducers/taskReducer.js
@@ -3,27 +3,27 @@ import { toast } from "react-toastify";
 const taskReducer = (state = {}, action) => {
   switch (action.type) {
     case "ADD_TASK": {
-      toast.success("Added a task");
+      toast.success("Added");
       return state;
     }
     case "ADD_TASK_ERR": {
-      toast.error("An error occurred");
+      toast.error("Error occurred");
       return state;
     }
     case "REMOVE_TASK": {
-      toast.warn("A task was removed...");
+      toast.warn("Removed");
       return state;
     }
     case "REMOVE_TASK_ERR": {
-      toast.error("A task remove error occured....");
+      toast.error("Remove error occured....");
       return state;
     }
     case "TOGGLE_CHECKED": {
-      toast.info("A task status changed...");
+      toast.info("Task Status Changed");
       return state;
     }
     case "TOGGLE_CHECKED_ERR": {
-      toast.error("A task status change error occured...");
+      toast.error("Task status change error occured...");
       return state;
     }
     default:
